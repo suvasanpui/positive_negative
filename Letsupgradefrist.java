d78e874e27eebbe3d9957729af725f08d1f31634
suvasanpui <127007312+suvasanpui@users.noreply.github.com>
2023-03-30 21:08:40 +0530

    Add files via upload

---
+++ Letsupgradefrist.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+public class Letsupgradefrist {
+    public static void main(String [] args)
+    {
+        Scanner obj=new Scanner(System.in);
+        int num;
+        System.out.println("Enter a number ");
+        num=obj.nextInt();
+        if(num>0)
+        {
+            System.out.println("These number is positive number ");
+    
+        }else{
+            System.out.println("This number is negative number ");
+        }
+     
+    }
+    
+}
